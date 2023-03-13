03ec90b679ee201185f7d68e55b691c49b5ff2ab
skyzavr <dankozero111@gmail.com>
2023-03-13 22:39:27 +0300

    add: responsive for header and content of main page

--- src/components/Pages/Elements/Header/header.js
+++ src/components/Pages/Elements/Header/header.js
@@ -1,9 +1,18 @@
 import classes from './header.module.css';
 import Github from '../../../assets/images/logo/github';
 import logo from '../../../assets/images/logo/rdr2Logo.png';
+import line from '../../../assets/images/others/line.png';
+import halfLine from '../../../assets/images/others/halfLine.png';
+import { useState } from 'react';
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuHadler = () => {
+    setOpenMenu((prev) => !prev);
+  };
   return (
-    <header className={classes.container}>
+    <header
+      className={`${classes.container} ${openMenu ? classes.menuOpen : ''}`}
+    >
       <div className={classes.github}>
         <a
           href="https://github.com/skyzavr/RDR2_spa_characters"
@@ -12,8 +21,16 @@ const Header = () => {
           <Github />
         </a>
       </div>
-      <nav className={classes.menu}>
-        <ul>
+      <nav className={`${classes.menu} `}>
+        <button className={classes.navBtn} onClick={menuHadler}>
+          <div>
+            <img src={line} alt="line logo" />
+          </div>
+          <div>
+            <img src={halfLine} alt="half line logo" />
+          </div>
+        </button>
+        <ul className={openMenu ? classes.navLinks : ''}>
           <li>
             <a href="#Characters">Characters</a>
           </li>
@@ -28,6 +45,7 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+
       <div className={classes.logo}>
         <img src={logo} alt="RDR2 logo" />
       </div>
